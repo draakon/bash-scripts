eec9bf13314111bcca23e378966584cfbe9263dc
Kristjan Kaitsa <draakon@masendav.org>
2014-02-14 09:33:22 +0200

    Handle errors and continue with script

--- bitcoin-status.sh
+++ bitcoin-status.sh
@@ -2,8 +2,8 @@
 #title			:bitcoin-status..sh
 #description	:This scripts continuously displays bitcoin price from Bitstamp.net market (value of one BTC in USD) and the latest block height and age from BlockChain.info.
 #author			:Kristjan Kaitsa
-#date			:20140213
-#version		:1.2.0
+#date			:20140214
+#version		:1.2.1
 #usage:			:bash bitcoin-status.sh
 #notes:			:Install cURL and Jshon to use the script. Press Ctrl+C to exit script.
 #bash_version:	:4.2-5ubuntu3
@@ -32,20 +32,33 @@ COUNTER=0
 
 # don't want to use 'watch', because it runs in full screen mode
 while true; do
-	if [ "$SKIP_BITSTAMP_INTERVALS" -eq 0 ] || [ "$(($COUNTER % ($SKIP_BITSTAMP_INTERVALS + 1)))" -eq 0 ] ; then
-		PRICE=`curl --get --silent $BITSTAMP_API_URL | jshon -e last -u`
+	if [ "$PRICE" == "FAILED" ] || [ "$SKIP_BITSTAMP_INTERVALS" -eq 0 ] || [ "$(($COUNTER % ($SKIP_BITSTAMP_INTERVALS + 1)))" -eq 0 ] ; then
+		PRICE=`curl --get --silent $BITSTAMP_API_URL | jshon -e last -u 2> /dev/null || echo -1`
 	fi
-	if [ "$SKIP_BLOCKCHAIN_INVERVALS" -eq 0 ] || [ "$(($COUNTER % ($SKIP_BLOCKCHAIN_INVERVALS + 1)))" -eq 0 ] ; then
+	if [ "$HEIGHT" == "FAILED" ] || [ "$TIME_DELTA_HUMAN" == "FAILED" ] || [ "$SKIP_BLOCKCHAIN_INVERVALS" -eq 0 ] || [ "$(($COUNTER % ($SKIP_BLOCKCHAIN_INVERVALS + 1)))" -eq 0 ] ; then
 		BLOCKCHAIN_OUTPUT=`curl --get --silent $BLOCKCHAIN_API_URL`
-		HEIGHT=`echo $BLOCKCHAIN_OUTPUT | jshon -e height -u`
-		TIME=`echo $BLOCKCHAIN_OUTPUT | jshon -e time -u`
+		HEIGHT=`echo $BLOCKCHAIN_OUTPUT | jshon -e height -u 2> /dev/null || echo -1`
+		TIME=`echo $BLOCKCHAIN_OUTPUT | jshon -e time -u 2> /dev/null || echo -1`
 	fi
 	
-	TIME_CURRENT_UTC=`TZ='UTC' date +%s`
-	TIME_DELTA=`echo "$TIME_CURRENT_UTC-$TIME" | bc`
-	TIME_DELTA_HUMAN="$(($TIME_DELTA / 60))m $(($TIME_DELTA % 60))s"
+	if [ "$PRICE" == "-1" ] ; then
+		echo -en "\nExtracting latest price information failed."
+		PRICE="FAILED"
+	else
+		PRICE="$"$PRICE
+	fi
+
+	if [ "$HEIGHT" == "-1" ] || [ "$TIME" == "-1" ] ; then
+		echo -en "\nExtracting latest block information failed."
+		HEIGHT="FAILED"
+		TIME_DELTA_HUMAN="FAILED"
+	else
+		TIME_CURRENT_UTC=`TZ='UTC' date +%s`
+		TIME_DELTA=`echo "$TIME_CURRENT_UTC-$TIME" | bc`
+		TIME_DELTA_HUMAN="$(($TIME_DELTA / 60))m $(($TIME_DELTA % 60))s"
+	fi
 	
-	echo -ne "\n[" && echo -n `date +"$TIME_FORMAT"` && echo -ne "] LAST PRICE: $"$PRICE"\tHEIGHT: "$HEIGHT"\tAGE: "$TIME_DELTA_HUMAN
+	echo -ne "\n[" && echo -n `date +"$TIME_FORMAT"` && echo -ne "] LAST PRICE: "$PRICE"\tHEIGHT: "$HEIGHT"\tAGE: "$TIME_DELTA_HUMAN
 	
 	sleep $INTERVAL
 	
